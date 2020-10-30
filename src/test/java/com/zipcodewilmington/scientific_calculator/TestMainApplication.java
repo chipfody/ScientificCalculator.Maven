37894c83752868bb27630318ac7a8d27decd7fd5
chipfody <60615532+chipfody@users.noreply.github.com>
2020-10-30 17:13:01 -0400

    modifying methods

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,16 +1,20 @@
 package com.zipcodewilmington.scientific_calculator;
 
 import com.zipcodewilmington.scientificcalculator.Calculator;
+import org.junit.Assert;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
     @Test
-    public void test1() {
+        public void test1() {
         Calculator c1 = new Calculator();
-        assertEquals(55.0, c1.add(10.0, 45.0), 0.01);
+        Assert.assertEquals(55.0, c1.add(10.0, 45.0), 0.01);
         assertEquals(2.0, c1.add(-5.0, 7.0), 0.01);
         assertEquals(-290.0, c1.add(-25.0, -265.0), 0.01);
         assertEquals(100012345.0, c1.add(100000000, 12345.0), 0.01);
